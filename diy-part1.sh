768ad38e60daf2596f989678a92084ba6cd6caf7
moneyfly1 <93109844+moneyfly1@users.noreply.github.com>
2021-11-14 13:31:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,14 +15,14 @@
 # sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
 #获取Lienol-xiaorouji-passwall
-# git clone https://github.com/xiaorouji/openwrt-package/lienol/ package/diy-packages/lienol
-# git clone https://github.com/xiaorouji/openwrt-package/tree/master/lienol/luci-app-passwall package/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-package/lienol/ package/diy-packages/lienol
+git clone https://github.com/xiaorouji/openwrt-package/tree/master/lienol/luci-app-passwall package/luci-app-passwall
 git clone https://github.com/kenzok8/openwrt-packages.git package/diy-packages
 git clone https://github.com/kenzok8/small.git package/small
 
 # 增加ssr
-# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-# git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
 
 #  增加luci-app-unblockneteasemusic，来源immortalwrt
 git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
